fd36ccdf884e2960c77a8af948a33050d53ff8db
XiayuTang <tangxg16@gmail.com>
2019-04-18 10:22:17 +0800

    init

--- test.jl
+++ test.jl
@@ -1,1 +1,16 @@
 # test codes and functions here
+# how to use debugger on Juno
+# step1: make sure there is a function called main() which runs your
+# entire program.
+# step2: using Revise, Juno
+# step3: includet("Your File Name")
+# step3: Juno.@enter main() or Juno.@run main()
+function main()
+    a = 10
+    b = 20
+    A = [1 3 4 5]
+    B = rand(4,4)
+    α = a*A
+    β = b*B
+    α * β
+end
